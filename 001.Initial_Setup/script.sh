c6b8e4210ca190662d46f79d5f1d331858edec91
Romain Deville <rdeville>
2016-02-24 09:54:41 +0100

    WIP on script

--- 001.Initial_Setup/script.sh
+++ 001.Initial_Setup/script.sh
@@ -16,17 +16,20 @@ source ../function/ask_continue.sh
 # Boolean for verbose
 VERBOSE=false
 VERBOSE_STEP=false
-# Usefull variable
+
+# More explicit variable
 SCRIPT=$0
 
-
+###############################################################################
+#                                 LOG & HELP                                  #
+###############################################################################
 usage() {
     # Usage  : usage
     # Input  : None
     # Output : None
     # Brief  : Print usage of this script to stdout
     cat <<- EOM
-    Usage: ${SCRIPT} [OPTION]"
+    Usage: ${SCRIPT} [OPTION]
 
     Available option :
         -v : Verbose level 1. Only print current function called
@@ -41,16 +44,16 @@ usage() {
 EOM
 }
 
-# Verbose parsing to know witch output to print
+
 verbose () {
     # Usage  : verbose <FUNCNAME>
     # Input  :
     #   $1-<FUNCNAME> : Function that call this
     # Output : None
-    # Desc   : Print log to stdout
+    # Desc   : Verbose parsing to know witch log to print to stdout
     if [ "$#" -ne 1  ]
     then
-        echo "[WARNING] - Calling $0 without argument"
+        echo "[WARNING] - Calling ${FUNCNAME} without the right number of argument"
     else
         if [[ ${VERBOSE} == true ]]
         then
@@ -62,96 +65,272 @@ verbose () {
         fi
     fi
 }
+# End of verbose()
 
-# Update Distrib and package
+###############################################################################
+#                              ALL USER CONFIG                                #
+###############################################################################
 full_update() {
     # Usage  : full_update
     # Input  : None
     # Output : None
     # Brief  : Update source, upgrade all package and upgrade distrib
-    if [ "$#" -ne 1  ]
-    then
     verbose ${FUNCNAME}
     sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
 }
+# End of full_update()
 
-# Ask to change root password
+set_sudoers_rootpw() {
+    # Usage  : set_sudoers_rootpw
+    # Input  : None
+    # Output : None
+    # Brief  : 
+    #   Change sudoer to ask root passwd instead of user one and make a backup
+    #   of old sudoers file.
+    verbose ${FUNCNAME}
+    echo "Set sudo passwd to root"
+    ask_continue ${FUNCNAME}
+    if [[ $yn == [Yy] ]]
+    then
+      sudo sed -i.bak -e "\$aDefaults rootpw" /etc/sudoers
+    fi
+}
+# End of set_sudoers_rootpw()
+
+add_repo_awesome3.5() {
+    # Usage  : add_repo_awesome3.5
+    # Input  : None
+    # Output : None
+    # Brief  : Add ppa for awesome 3.5 on ubuntu 14.04
+    verbose ${FUNCNAME}
+    echo "Add ppa for awesome-3.5"
+    ask_continue ${FUNCNAME}
+    if [[ $yn == [Yy] ]]
+    then
+        sudo add-apt-repository ppa:klaus-vormweg/awesome
+        sudo apt-get update
+    else 
+        AWESOME_WM=false
+    fi
+}
+# End of add_repo_awesome3.5
+
+install_base_pkg() {
+    # Usage  : install_base_pkg
+    # Input  : None
+    # Output : None
+    # Brief  : Instal minimal usefull soft
+    verbose ${FUNCNAME}
+    if $AWESOME_WM
+    then
+        sudo apt-get install \
+          vim \
+          git \
+          mr \
+          vcsh \
+          zsh \
+          keychain \
+          xclip \
+          awesome \
+          awesome-extra \
+          terminator
+    else
+        sudo apt-get install \
+          vim \
+          git \
+          mr \
+          vcsh \
+          zsh \
+          keychain \
+          xclip \
+          terminator
+    fi
+}
+# End of install_base_pkg
+
+setup_distrib()
+{
+  full_update
+  set_sudoers_rootpw
+  add_repo_awesome3.5
+  install_base_pkg
+}
+# Enf of setup_distrib()
+
+
+###############################################################################
+#                           USER SPECIFIC CONFIG                              #
+###############################################################################
 set_user_pwd() {
     # Usage  : set_user_pwd <USER>
-    verbose ${FUNCNAME}
-    echo "Please change root password."
-    ask_continue $1
-    su -c 'passwd'
+    # Input  : 
+    #   $1-<USER> : User account name 
+    # Output : None
+    # Brief  : Ask new password for <USER>
+    if [ "$#" -ne 1  ]
+    then
+        echo "[WARNING] - Calling ${FUNCNAME} without the right number of argument"
+    else
+        verbose ${FUNCNAME}
+        echo "Please change $1 password."
+        ask_continue ${FUNCNAME}
+        if [[ $yn == [Yy] ]]
+        then
+            su $1 -c 'passwd'
+        fi
+    fi
 }
+# End of set_user_pwd()
 
-# Change sudoer to ask root passwd instead of user one
-set_sudoers_rootpw() {
-  verbose ${FUNCNAME}
-  sudo sed -i.bak -e "\$aDefaults rootpw" /etc/sudoers
-}
-
-# Add ppa for awesome 3.5 on ubuntu 14.04
-add_repo_awesome3-5() {
-  verbose ${FUNCNAME}
-  sudo add-apt-repository ppa:klaus-vormweg/awesome
-  sudo apt-get update
-}
-
-#Setup usefull littlelittle soft
-install_base_pkg() {
-  verbose ${FUNCNAME}
-  sudo apt-get install \
-    vim \
-    git \
-    mr \
-    vcsh \
-    zsh \
-    keychain \
-    xclip \
-    awesome \
-    awesome-extra \
-    terminator
-}
-
-# Add git config
 setup_git_config() {
-    verbose ${FUNCNAME}
-    # Usage : setup_git_config <USER> 
-    su $1 -c "git config --global user.name '${USER_FNAME} ${USER_LNAME}'"
-    su $1 -c "git config --global user.email '${USER_MAIL}'"
-    su $1 -c "git config --global push.default matching"
+    # Usage  : setup_git_config <USER> <USER_FNAME> <USER_LNAME> <USER_MAIL>
+    # Input  : 
+    #   $1-<USER>       : User account name 
+    #   $2-<USER_FNAME> : User first name
+    #   $3-<USER_LNAME> : User last name
+    #   $4-<USER_MAIL>  : User email
+    # Output : None
+    # Brief  : Set  git config globally for <USER>
+    if [ "$#" -ne 4  ]
+    then
+        echo "[WARNING] - Calling ${FUNCNAME} without the right number of argument"
+    else
+        verbose ${FUNCNAME}
+        echo "Set user $1 git config."
+        ask_continue  ${FUNCNAME}
+        if [[ $yn == [Yy] ]]
+        then 
+            su $1 -c "git config --global user.name '$2 $3'"
+            su $1 -c "git config --global user.email '$4'"
+            su $1 -c "git config --global push.default matching"
+        fi
+    fi
 }
+# End of setup_git_config
 
-# Generate Keygen 
 generate_ssh_key () {
-    verbose ${FUNCNAME}
-    # Usage : generate_ssh_key <USER>
-    su $1 -c "ssh-keygen -t rsa -b 4096 -C '$USER_MAIL'"
-    # Add  key to ssh-agent
-    su $1 -c "eval '$(ssh-agent -s)'"
-    su $1 -c "sh-add ~/.ssh/id_rsa"
-    # Copy public key to put it into account (gitlab, github, bitbucket ...)
-    su $1 -c "xclip -sel clip < ~/.ssh/id_rsa.pub"
-
-    echo "The content of your public ssh key is stored into your clipboard"
-    echo "Please put them into your account (github, gitlab, bitbucket ...)"
-    ask_continue ${FUNCNAME}
+    # Usage  : generate_ssh_key <USER> <USER_MAIL>
+    # Input  : 
+    #   $1-<USER>       : User account name 
+    #   $2-<USER_MAIL>  : User email
+    # Output : None
+    # Brief  : Setup ssh key for <USER>
+    if [ "$#" -ne 2  ]
+    then
+        echo "[WARNING] - Calling ${FUNCNAME} without the right number of argument"
+    else
+        verbose ${FUNCNAME}
+        echo "Generate ssh key for user $1"
+        ask_continue ${FUNCNAME}
+        if [[ $yn == [Yy] ]]
+        then 
+            su $1 -c "ssh-keygen -t rsa -b 4096 -C '$2'"
+            # Add  key to ssh-agent
+            su $1 -c "eval '$(ssh-agent -s)'"
+            su $1 -c "ssh-add ~/.ssh/id_rsa"
+            su $1 -c "cat ~/.ssh/id_rsa.pub"
+            echo "Above is the content of $1 public ssh key"
+            echo "Please copy it and put them into your account (github,...)"
+            ask_continue ${FUNCNAME}
+        fi
+    fi
 }
+# End of setup_git_config
 
-# Get back your dotfile
 clone_dotfiles() {
-    # Usage : clone_dotfile <USER>
-    verbose ${FUNCNAME}
-    su $1 -c "vcsh clone git@bitbucket.org:vcsh/mr.git"
-    su $1 -c "mkdir ~/.log"
-    su $1 -c "mr up"
+    # Usage : clone_dotfile <USER> <DOTFILE_LOC>
+    # Input  : 
+    #   $1-<USER>         : User account name 
+    #   $2-<DOTFILE_LOC>  : Location of dotfiles of the form git@ or https://
+    # Output : None
+    # Brief  : Get dotfiles from server <USER>
+    if [ "$#" -ne 2 ]
+    then
+        echo "[WARNING] - Calling ${FUNCNAME} without the right number of argument"
+    else
+        verbose ${FUNCNAME}
+        echo "Get dotfiles from $2."
+        ask_continue ${FUNCNAME}
+        if [[ $yn == [Yy] ]]
+        then 
+          su $1 -c "vcsh clone $2"
+          su $1 -c "mkdir ~/.log"
+          su $1 -c "mr up"
+        fi
+    fi
 }
+# End of clone_dotfiles()
 
 # Change shell
-chg_shell () {
-     verbose ${FUNCNAME}
-     su $1 -c "chsh -s /bin/zsh"
+chg_shell() {
+    # Usage : chg_shell <USER>
+    # Input  : 
+    #   $1-<USER>       : User account name 
+    # Output : None
+    # Brief  : Change shell for <USER>
+    if [ "$#" -ne 1 ]
+    then
+        echo "[WARNING] - Calling ${FUNCNAME} without the right number of argument"
+    else
+        verbose ${FUNCNAME}
+        echo "Change shell for user $1"
+        ask_continue ${FUNCNAME}
+        if [[ $yn == [Yy] ]]
+        then 
+            su $1 -c "chsh -s /bin/zsh"
+        fi
+    fi
 }
+# End of chg_shell()
+
+setup_user() {
+    # Usage  : setup_user <USER> <USER_FNAME> <USER_LNAME> <USER_MAIL> <DOTFILE_LOC>
+    # Input  : 
+    #   $1-<USER>       : User account name 
+    #   $2-<USER_FNAME> : User first name
+    #   $3-<USER_LNAME> : User last name
+    #   $4-<USER_MAIL>  : User email
+    #   $5-<DOTFILE_LOC>  : Location of dotfiles of the form git@ or https://
+    # Output : None
+    # Brief  : Do all user specific configuration
+    DEFAULT_YN="Y"
+    echo "Does this informations are correct : [y/n] "
+    echo " User account name : $1 "
+    echo " User full name    : $2 $3 "
+    echo " User email        : $4"
+    echo " Dotfiles location : $5"
+    read yn
+    yn=${yn:-$DEFAULT_YN}
+
+    if [[ $yn == [Yy] ]]
+    then 
+        if id -u "$1" >/dev/null 2>&1
+        then
+            echo "User $1 exists, will not be created"
+            ask_continue ${FUNCNAME}
+        else
+            echo "User $1 does not exist, will be created"
+            ask_continue ${FUNCNAME}
+            if [[ $yn == [Yy] ]]
+            then
+                adduser $1
+            fi
+        fi
+
+        if [[ $yn == [Yy] ]]
+        then
+            set_user_pwd $1
+            setup_git_config $1 $2 $3 $4 
+            generate_ssh_key $1 $4
+            clone_dotfiles $1 $5
+            chg_shell $1
+        fi
+    else
+        echo "Please do review config file."
+        return
+    fi
+}
+# End of setup_user
 
 
 # Argument parser
@@ -173,4 +352,16 @@ while getopts ":vsh" opt; do
     h)
       usage
   esac
+done
+
+###############################################################################
+#                                   MAIN                                      #
+###############################################################################
+
+setup_distrib
+
+NB_USER=${#USER_ACCOUNT[@]}
+for (( i=0; i<${NB_USER}; i++ ))
+do
+    setup_user ${USER_ACCOUNT[$i]} ${USER_FNAME[$i]} ${USER_LNAME[$i]} ${USER_MAIL[$i]} ${DOTFILE_LOC[$i]}
 done
--- function/ask_continue.sh
+++ function/ask_continue.sh
@@ -2,18 +2,13 @@
 
 ask_continue() {
 	# In case of aborting save callback function to know from which point to 
-  # to continue in case of aborting.
+  	# to continue in case of aborting.
+  	local DEFAULT_YN="Y"
 	CALLBACK=$1
-	read -p 'Ready to continue ? [Y/n]' yn;
-  echo $yn
-	if [[ ! $yn == [NnYy] ]]
-	then
-		echo 'Please enter Yy or Nn'
-    ask_continue
-	elif [[ $yn == [Nn] ]]
+	read -p 'Ready to continue ? [Y/n]' yn
+	yn=${yn:-$DEFAULT_YN}
+	if [[ $yn == [Nn] ]]
 	then 
-		echo "$(date) : User do not want to continue from ${CALLBACK}" >> setup.log
+		echo "[LOG] - $(date) : User do not want to continue from ${CALLBACK}" >> setup.log
 	fi
-  exit
-
 }
