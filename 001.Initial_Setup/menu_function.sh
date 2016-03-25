eaec8179d9fe197b55c5f22ee6ed50d6be8929f0
Romain Deville <rdeville>
2016-03-25 15:26:46 +0100

    Make menu for category generic

---
+++ 001.Initial_Setup/menu_function.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+ALL_PKG_CHOOSEN=""
+ALL_APP_CAT=""
+
+# FROM RASPI-CONFIG
+calc_wt_size() {
+  # NOTE: it's tempting to redirect stderr to /dev/null, so supress error 
+  # output from tput. However in this case, tput detects neither stdout or 
+  # stderr is a tty and so only gives default 80, 24 values
+  WT_HEIGHT=17
+  WT_WIDTH=$(tput cols)
+
+  if [ -z "$WT_WIDTH" ] || [ "$WT_WIDTH" -lt 60 ]
+  then
+    WT_WIDTH=80
+  fi
+  if [ "$WT_WIDTH" -gt 178 ]
+  then
+    WT_WIDTH=120
+  fi
+  WT_MENU_HEIGHT=$(($WT_HEIGHT-7))
+}
+
+do_menu () {
+  # USAGE = do_menu <CAT>
+  # INPUT : 
+  #   CAT : Category of application. Must be the same as the file menu_${CAT}.sh and APP_${CAT}.sh
+
+  local NAME=$1
+  local LOWER_NAME=`echo "${NAME}" | tr '[:upper:]' '[:lower:]'`
+  local UPPER_NAME=`echo "${NAME}" | tr '[:lower:]' '[:upper:]'`
+  
+  source menu_${LOWER_NAME}.sh
+  
+  local ARR_NAME="APP_${NAME}_NAME[@]"
+  local ARR_PKG="APP_${NAME}_PKG[@]"
+  local ARR_DESC="APP_${NAME}_DESC[@]"
+  local ARR_STAT="APP_${NAME}_STAT[@]"
+  
+  local APP_ARR_NAME=("${!ARR_NAME}")
+  local APP_ARR_PKG=("${!ARR_PKG}")
+  local APP_ARR_DESC=("${!ARR_DESC}")
+  local APP_ARR_STAT=("${!ARR_STAT}")
+
+  local NB_APP=$(( ${#APP_ARR_NAME[@]} ))
+
+  calc_wt_size
+  
+  local MENU_WM="whiptail --title 'Window Manager' --checklist  'Which window manager to install :' \
+                  $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT"
+  for (( idx=0 ; idx <= ${NB_APP}-1 ; idx++ ))
+  do
+    MENU_WM="${MENU_WM} '${APP_ARR_NAME[${idx}]}' '${APP_ARR_DESC[${idx}]}' '${APP_ARR_STAT[${idx}]}'"
+  done
+
+  bash -c "${MENU_WM}" 2> results_menu.txt
+
+  CHOICE=$( cat results_menu.txt )
+  
+  for (( idx=0 ; idx <= ${NB_APP}-1 ; idx++ ))
+  do
+    if echo ${CHOICE} | grep "\"${APP_ARR_NAME[${idx}]}\""
+    then  
+      CMD="sed -i 's/STAT\[${idx}\]=\"\(OFF\|ON\)\"/STAT\[${idx}\]=\"ON\"/g' menu_${LOWER_NAME}.sh"
+      eval ${CMD}
+    else
+      CMD="sed -i 's/STAT\[${idx}\]=\"\(OFF\|ON\)\"/STAT\[${idx}\]=\"OFF\"/g' menu_${LOWER_NAME}.sh"
+      eval ${CMD}
+    fi
+  done
+}
--- 001.Initial_Setup/menu_wm.sh
+++ 001.Initial_Setup/menu_wm.sh
@@ -1,66 +1,66 @@
 #!/bin/bash
 
-source setup_pkg_menu.sh
+ALL_APP_CAT+='WM'
+
 
 declare -A APP_WM
 
 APP_WM_WIDTH=4
 
-APP_WM[1,1]="Xubuntu_Desktop"
-APP_WM[1,2]="xubuntu-desktop" # TODO
-APP_WM[1,3]="Full well known desktop manager based on xfce"
-APP_WM[1,4]="OFF"
+APP_WM_NAME[0]="Xubuntu_Desktop"
+APP_WM_PKG[0]="xubuntu-desktop" # TODO
+APP_WM_DESC[0]="Full well known desktop manager based on xfce"
+APP_WM_STAT[0]="OFF"
 
 Xubuntu_Desktop_routine () {
   # TODO Parse ALL_PKG_CHOOSEN to be sure it does not belong to it
-  APP_WM[1,4]="ON"
-  ALL_PKG_CHOOSEN+="${#APP_WM[@]} " 
+  echo
 }
 
-APP_WM[2,1]="Ubuntu-Desktop"
-APP_WM[2,2]="ubuntu-desktop" # TODO
-APP_WM[2,3]="Full well known desktop manager based on unity"
-APP_WM[2,4]="OFF"
+APP_WM_NAME[1]="Ubuntu-Desktop"
+APP_WM_PKG[1]="ubuntu-desktop" # TODO
+APP_WM_DESC[1]="Full well known desktop manager based on unity"
+APP_WM_STAT[1]="OFF"
 
-APP_WM[3,1]="Kubuntu-Desktop"
-APP_WM[3,2]="kubuntu-desktop" # TODO
-APP_WM[3,3]="Full well known desktop manager based on KDE"
-APP_WM[3,4]="OFF"
+APP_WM_NAME[2]="Kubuntu-Desktop"
+APP_WM_PKG[2]="kubuntu-desktop" # TODO
+APP_WM_DESC[2]="Full well known desktop manager based on KDE"
+APP_WM_STAT[2]="OFF"
 
-APP_WM[4,1]="Awesome_3.4"
-APP_WM[4,2]="awesome" 
-APP_WM[4,3]="A light and very customable WM 3.5"
-APP_WM[4,4]="OFF"
+APP_WM_NAME[3]="Awesome_3.4"
+APP_WM_PKG[3]="awesome" 
+APP_WM_DESC[3]="A light and very customable WM 3.5"
+APP_WM_STAT[3]="OFF"
 
-APP_WM[5,1]="Awesome_3.5"
-APP_WM[5,2]="awesome"
-APP_WM[5,3]="A light and very customable WM v3.5"
-APP_WM[5,4]="OFF"
+APP_WM_NAME[4]="Awesome_3.5"
+APP_WM_PKG[4]="awesome"
+APP_WM_DESC[4]="A light and very customable WM v3.5"
+APP_WM_STAT[4]="OFF"
 
-APP_WM[6,1]="KDE"
-APP_WM[6,2]="kde" # TODO
-APP_WM[6,3]="Full desktop environnement, graphical, easy to use."
-APP_WM[6,4]="OFF"
+APP_WM_NAME[5]="KDE"
+APP_WM_PKG[5]="kde" # TODO
+APP_WM_DESC[5]="Full desktop environnement, graphical, easy to use."
+APP_WM_STAT[5]="OFF"
 
-APP_WM[7,1]="LXQt"
-APP_WM[7,2]="lxqt" # TODO
-APP_WM[7,3]="Ligthweight desktop environnement, modular, portable"
-APP_WM[7,4]="OFF"
+APP_WM_NAME[6]="LXQt"
+APP_WM_PKG[6]="lxqt" # TODO
+APP_WM_DESC[6]="Ligthweight desktop environnement, modular, portable"
+APP_WM_STAT[6]="OFF"
 
-APP_WM[8,1]="Gnome"
-APP_WM[8,2]="gnome" # TODO
-APP_WM[8,3]="Full desktop environnement, graphical, easy to use."
-APP_WM[8,4]="OFF"
+APP_WM_NAME[7]="Gnome"
+APP_WM_PKG[7]="gnome" # TODO
+APP_WM_DESC[7]="Full desktop environnement, graphical, easy to use."
+APP_WM_STAT[7]="OFF"
 
-APP_WM[9,1]="Mate"
-APP_WM[9,2]="mate" # TODO
-APP_WM[9,3]="Full desktop environnement, graphical, easy to use. Fork from Gnome"
-APP_WM[9,4]="OFF"
+APP_WM_NAME[8]="Mate"
+APP_WM_PKG[8]="mate" # TODO
+APP_WM_DESC[8]="Full desktop environnement, graphical, easy to use. Fork from Gnome"
+APP_WM_STAT[8]="OFF"
 
-APP_WM[10,1]="XFCE_4"
-APP_WM[10,2]="xfce4" # TODO
-APP_WM[10,3]="Ligthweight desktop environnement, modular, portable"
-APP_WM[10,4]="OFF"
+APP_WM_NAME[9]="XFCE_4"
+APP_WM_PKG[9]="xfce4" # TODO
+APP_WM_DESC[9]="Ligthweight desktop environnement, modular, portable"
+APP_WM_STAT[9]="OFF"
 
 #add_repo_awesome3.5() {
     # Usage  : add_repo_awesome3.5
@@ -79,93 +79,3 @@ APP_WM[10,4]="OFF"
     #fi
 #}
 # End of add_repo_awesome3.5
-
-install_base_pkg() {
-    # Usage  : install_base_pkg
-    # Input  : None
-    # Output : None
-    # Brief  : Instal minimal usefull soft
-    verbose ${FUNCNAME}
-    sudo apt-get install \
-        apt-transport-https
-    if $AWESOME_WM
-    then
-        sudo apt-get install \
-          vim \
-          git \
-          mr \
-          vcsh \
-          zsh \
-          keychain \
-          xclip \
-          awesome \
-          awesome-extra \
-          teWMinator \
-	  tree
-    else
-        sudo apt-get install \
-          vim \
-          git \
-          mr \
-          vcsh \
-          zsh \
-          keychain \
-          xclip \
-          teWMinator \
-	      tree
-    fi
-}
-# End of install_base_pkg
-
-
-wm_menu () {
-  # wm_menu
-  local NAME=$1
-  local APP_ARR="APP_${NAME}"
-  echo ${APP_ARR}
-  APP_ARR=${APP_ARR}
-  echo ${APP_ARR}
-  local NB_APP=$(( ${#APP_ARR[@]} / 4 ))
-  echo ${NB_APP}
-  
-  calc_wt_size
-  
-  local NB_WM=$(( ${#APP_WM[@]} / 4 ))
-
-  local MENU_WM="whiptail --title 'Window Manager' --checklist  'Which window manager to install :' $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT"
-  for (( idx=1 ; idx <= ${NB_WM} ; idx++ ))
-  do
-    MENU_WM="${MENU_WM} '${APP_WM[${idx},1]}' '${APP_WM[${idx},3]}' '${APP_WM[${idx},4]}'"
-  done
-
-  local CMD=''
-
-  eval "${MENU_WM}" 2> 'results_wm.txt'
-  
-  for CHOICE in $( cat results_wm.txt )
-  do
-    if [[ "${APP_WM[@]}" =~ "${CHOICE}" ]]
-    then 
-      return -1 
-    fi
-    idx=0
-    FOUND=false
-    while ! ${FOUND} 
-    do
-      if [[ "\"${APP_WM[${idx},1]}\"" == "${CHOICE}" ]]
-        then  
-          APP_WM[${idx},4]="ON"
-          CMD="${APP_WM[${idx},1]}_routine"
-          ${CMD}
-          FOUND=true
-      fi
-      idx=$(( idx + 1 ))
-      if [[ ${idx} == $(( ${#APP_WM[@]} / $APP_WM_WIDTH + $APP_WM_WIDTH  )) ]]
-      then
-        return -1 
-      fi
-    done
-  done
-}
-
-wm_menu "WM"
