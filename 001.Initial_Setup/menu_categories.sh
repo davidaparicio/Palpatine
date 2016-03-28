d45f694900e4cdeb46ff9d775558348474515264
Your Name <your@email.com>
2016-03-28 03:08:11 +0200

    Update autogenerated menu_cate

--- 001.Initial_Setup/menu_categories.sh
+++ 001.Initial_Setup/menu_categories.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  
+#!/bin/bash  \n\n
 # BEGIN WM INFO
 ALL_APP_CAT+=":WM"
 APP_WM_CAT="Window Manager"
@@ -19,3 +19,18 @@ ALL_APP_CAT+=':EDITOR'
 APP_EDITOR_CAT="Text Editor"
 APP_EDITOR_EX="A set of text editor"
 # END WM INFO
+# BEGIN VC INFO
+ALL_APP_CAT+=':VC'
+APP_VC_CAT="Version Control"
+APP_VC_EX="Version Control System (VCS)"
+# end WM INFO
+# BEGIN DEV INFO
+ALL_APP_CAT+=':DEV'
+APP_DEV_CAT="Developpement tools"
+APP_DEV_EX="Developpement tools"
+# end DEV INFO
+# BEGIN FM INFO
+ALL_APP_CAT+=':FM'
+APP_FM_CAT="File Manager"
+APP_FM_EX="File Manager"
+# end FM INFO
