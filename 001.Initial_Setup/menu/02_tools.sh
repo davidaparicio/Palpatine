be422a7cf7371e24f2638179609bdc4afac49c42
Your Name <your@email.com>
2016-03-28 02:12:34 +0200

    Add som app

--- 001.Initial_Setup/menu/02_tools.sh
+++ 001.Initial_Setup/menu/02_tools.sh
@@ -7,27 +7,31 @@ APP_TOOLS_EX="Some usefull tools, utility and accessories."
 # END WM INFO
 
 APP_TOOLS_NAME[0]="htop"
-APP_TOOLS_PKG[0]="htop" 
+APP_TOOLS_PKG[0]="htop"
 APP_TOOLS_DESC[0]="An interactive process viewer for Unix systems"
 APP_TOOLS_STAT[0]="ON"
 
 APP_TOOLS_NAME[1]="gparted"
-APP_TOOLS_PKG[1]="gparted" 
+APP_TOOLS_PKG[1]="gparted"
 APP_TOOLS_DESC[1]="A free partition editor for graphically managing your disk partitions"
 APP_TOOLS_STAT[1]="ON"
 
 APP_TOOLS_NAME[2]="git"
-APP_TOOLS_PKG[2]="git" 
+APP_TOOLS_PKG[2]="git"
 APP_TOOLS_DESC[2]="Version Control System"
 APP_TOOLS_STAT[2]="ON"
 
 APP_TOOLS_NAME[3]="vcsh"
-APP_TOOLS_PKG[3]="vcsh" 
+APP_TOOLS_PKG[3]="vcsh"
 APP_TOOLS_DESC[3]="Version Control System for \$HOME"
 APP_TOOLS_STAT[3]="ON"
 
 APP_TOOLS_NAME[4]="myRepo"
-APP_TOOLS_PKG[4]="mr" 
+APP_TOOLS_PKG[4]="mr"
 APP_TOOLS_DESC[4]="Multiple Repository management tool"
 APP_TOOLS_STAT[4]="ON"
 
+APP_TOOLS_NAME[4]="xclip"
+APP_TOOLS_PKG[4]="xclip"
+APP_TOOLS_DESC[4]="Command line clipboard"
+APP_TOOLS_STAT[4]="ON"
--- 001.Initial_Setup/menu/03_editor.sh
+++ 001.Initial_Setup/menu/03_editor.sh
@@ -7,6 +7,41 @@ APP_EDITOR_EX="A set of text editor"
 # END WM INFO
 
 APP_EDITOR_NAME[0]="vi"
-APP_EDITOR_PKG[0]="vi" 
+APP_EDITOR_PKG[0]="vi"
 APP_EDITOR_DESC[0]="vi is a screen-oriented text editor originally"
 APP_EDITOR_STAT[0]="OFF"
+
+APP_EDITOR_NAME[1]="vim"
+APP_EDITOR_PKG[1]="vim"
+APP_EDITOR_DESC[1]="vi iMproved is a screen-oriented text editor originally"
+APP_EDITOR_STAT[1]="ON"
+
+APP_EDITOR_NAME[2]="emacs"
+APP_EDITOR_PKG[2]="emacs"
+APP_EDITOR_DESC[2]="A screen-based editor with an embedded computer language"
+APP_EDITOR_STAT[2]="OFF"
+
+APP_EDITOR_NAME[3]="nano"
+APP_EDITOR_PKG[3]="nano"
+APP_EDITOR_DESC[3]="A screen-based editor"
+APP_EDITOR_STAT[3]="OFF"
+
+APP_EDITOR_NAME[4]="KWrite"
+APP_EDITOR_PKG[4]="kwrite"
+APP_EDITOR_DESC[4]="KWrite is a simple text editor built on the KDE Platform."
+APP_EDITOR_STAT[4]="OFF"
+
+APP_EDITOR_NAME[5]="Leafpad"
+APP_EDITOR_PKG[5]="leafpad"
+APP_EDITOR_DESC[5]="Leafpad is a simple text editor."
+APP_EDITOR_STAT[5]="OFF"
+
+APP_EDITOR_NAME[6]="Kate"
+APP_EDITOR_PKG[6]="kate"
+APP_EDITOR_DESC[6]="Kate is a text editor built for the KDE Platform."
+APP_EDITOR_STAT[6]="OFF"
+
+APP_EDITOR_NAME[7]="geany"
+APP_EDITOR_PKG[7]="geany"
+APP_EDITOR_DESC[7]="Geany is a light text editor."
+APP_EDITOR_STAT[7]="OFF"
