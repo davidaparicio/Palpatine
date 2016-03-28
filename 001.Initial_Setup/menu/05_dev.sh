3b26fd7341af84cea4b93731e757ff7db9ad565c
Romain Deville <romain.deville@insa-lyon.fr>
2016-03-28 03:06:48 +0200

    Add some app and categories based on ubuntu software center and my own preferred apps

---
+++ 001.Initial_Setup/menu/05_dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# BEGIN DEV INFO
+ALL_APP_CAT+=':DEV'
+APP_DEV_CAT="Developpement tools"
+APP_DEV_EX="Developpement tools"
+# end DEV INFO
+
+APP_DEV_NAME[0]="QtCreator"
+APP_DEV_PKG[0]="qtcreator"
+APP_DEV_DESC[0]="Qt Creator is a cross-platform C++, JavaScript and QML IDE"
+APP_DEV_STAT[0]="ON"
---
+++ 001.Initial_Setup/menu/06_fm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# BEGIN FM INFO
+ALL_APP_CAT+=':FM'
+APP_FM_CAT="File Manager"
+APP_FM_EX="File Manager"
+# end FM INFO
+
+APP_FM_NAME[0]="PcmanFM"
+APP_FM_PKG[0]="pcman"
+APP_FM_DESC[0]="Lightweight and fast file manager"
+APP_FM_STAT[0]="ON"
