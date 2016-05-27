26954d0732c047192b81d48a524bc109c56b1e7e
Romain Deville <romain.deville@insa-lyon.fr>
2016-05-27 19:52:33 +0200

    Fixed bug in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@
 cd ~/
 wget https://github.com/RDeville/Palpatine/archive/master.zip
 unzip master.zip
-cd palpatine-master
+cd Palpatine-master
 sudo ./main.sh
 rm -rf palpatine-master.zip setup.sh master.zip
